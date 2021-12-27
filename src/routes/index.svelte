27100e909388418dfa05db4ad8a01227492fba78
mwanginjamb <francnjamb@gmail.com>
2021-12-27 21:27:33 +0300

    Initial App Depemdancy Setup and Boostrap Integration

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="card">
+    <div class="card-header">
+        <h2 class="card-title">Cookie Based Authentication</h2>
+    </div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@import "src/variables.scss";'
+					}
+				}
+			}
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@import "src/variables.scss";'
+			}
+		})
+	]
 };
 
 export default config;
